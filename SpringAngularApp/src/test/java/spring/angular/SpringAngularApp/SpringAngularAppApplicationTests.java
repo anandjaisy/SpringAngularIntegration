7c0fc8f3c2d441152028a7bef1c25823961c2e16
anandjaisy <anandjaisy@gmail.com>
2025-03-31 08:30:08 +1100

    Spring boot application creation

---
+++ SpringAngularApp/src/test/java/spring/angular/SpringAngularApp/SpringAngularAppApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.angular.SpringAngularApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAngularAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
